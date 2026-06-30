013d75be4f8e52988de8942beab7cc75318b5d96
malackath <bernardo.arnelli@gmail.com>
2026-06-30 04:42:21 -0300

    Add files via upload

---
+++ cities-data.js
@@ -0,0 +1,470 @@
+module.exports = {
+  "artigas": [
+    {
+      "n": "Artigas",
+      "lat": -30.4094,
+      "lng": -56.47189
+    },
+    {
+      "n": "Bella Unión",
+      "lat": -30.26,
+      "lng": -57.59924
+    },
+    {
+      "n": "Tomás Gomensoro",
+      "lat": -30.43039,
+      "lng": -57.43868
+    },
+    {
+      "n": "Baltasar Brum",
+      "lat": -30.71604,
+      "lng": -57.32582
+    }
+  ],
+  "canelones": [
+    {
+      "n": "Canelones",
+      "lat": -34.52354,
+      "lng": -56.28319
+    },
+    {
+      "n": "Las Piedras",
+      "lat": -34.71759,
+      "lng": -56.21897
+    },
+    {
+      "n": "La Paz",
+      "lat": -34.75336,
+      "lng": -56.22983
+    },
+    {
+      "n": "Progreso",
+      "lat": -34.67305,
+      "lng": -56.22187
+    },
+    {
+      "n": "Ciudad de la Costa",
+      "lat": -34.84318,
+      "lng": -56.0122
+    },
+    {
+      "n": "Pando",
+      "lat": -34.71689,
+      "lng": -55.9604
+    },
+    {
+      "n": "Atlántida",
+      "lat": -34.7626,
+      "lng": -55.76339
+    },
+    {
+      "n": "La Floresta",
+      "lat": -34.75453,
+      "lng": -55.68614
+    },
+    {
+      "n": "Salinas",
+      "lat": -34.77054,
+      "lng": -55.84144
+    },
+    {
+      "n": "Santa Lucía",
+      "lat": -34.46517,
+      "lng": -56.38571
+    }
+  ],
+  "cerro-largo": [
+    {
+      "n": "Melo",
+      "lat": -32.36838,
+      "lng": -54.16884
+    },
+    {
+      "n": "Río Branco",
+      "lat": -32.59648,
+      "lng": -53.38258
+    },
+    {
+      "n": "Isidoro Noblía",
+      "lat": -31.96347,
+      "lng": -54.12543
+    }
+  ],
+  "colonia": [
+    {
+      "n": "Colonia del Sacramento",
+      "lat": -34.44777,
+      "lng": -57.8336
+    },
+    {
+      "n": "Carmelo",
+      "lat": -33.99488,
+      "lng": -58.28389
+    },
+    {
+      "n": "Nueva Helvecia",
+      "lat": -34.29564,
+      "lng": -57.23269
+    },
+    {
+      "n": "Juan Lacaze",
+      "lat": -34.42435,
+      "lng": -57.44569
+    },
+    {
+      "n": "Rosario",
+      "lat": -34.31547,
+      "lng": -57.35329
+    }
+  ],
+  "durazno": [
+    {
+      "n": "Durazno",
+      "lat": -33.38231,
+      "lng": -56.52365
+    },
+    {
+      "n": "Sarandí del Yi",
+      "lat": -33.33839,
+      "lng": -55.62942
+    },
+    {
+      "n": "Carmen",
+      "lat": -33.23696,
+      "lng": -56.00452
+    },
+    {
+      "n": "La Paloma",
+      "lat": -32.72526,
+      "lng": -55.58143
+    }
+  ],
+  "flores": [
+    {
+      "n": "Trinidad",
+      "lat": -33.51945,
+      "lng": -56.89823
+    },
+    {
+      "n": "Ismael Cortinas",
+      "lat": -33.96131,
+      "lng": -57.08857
+    }
+  ],
+  "florida": [
+    {
+      "n": "Florida",
+      "lat": -34.10167,
+      "lng": -56.22488
+    },
+    {
+      "n": "Sarandí Grande",
+      "lat": -33.72455,
+      "lng": -56.33005
+    },
+    {
+      "n": "25 de Agosto",
+      "lat": -34.41259,
+      "lng": -56.40526
+    }
+  ],
+  "lavalleja": [
+    {
+      "n": "Minas",
+      "lat": -34.3694,
+      "lng": -55.22822
+    },
+    {
+      "n": "José Pedro Varela",
+      "lat": -33.45589,
+      "lng": -54.54005
+    },
+    {
+      "n": "Solís de Mataojo",
+      "lat": -34.59621,
+      "lng": -55.46088
+    }
+  ],
+  "maldonado": [
+    {
+      "n": "Maldonado ciudad",
+      "lat": -34.89864,
+      "lng": -54.96292
+    },
+    {
+      "n": "Punta del Este",
+      "lat": -34.93662,
+      "lng": -54.93404
+    },
+    {
+      "n": "Piriápolis",
+      "lat": -34.86165,
+      "lng": -55.27322
+    },
+    {
+      "n": "San Carlos",
+      "lat": -34.78977,
+      "lng": -54.91885
+    },
+    {
+      "n": "Pan de Azúcar",
+      "lat": -34.77556,
+      "lng": -55.22902
+    },
+    {
+      "n": "Aiguá",
+      "lat": -34.20503,
+      "lng": -54.76232
+    },
+    {
+      "n": "Solís",
+      "lat": -34.78855,
+      "lng": -55.38347
+    },
+    {
+      "n": "Punta Negra",
+      "lat": -34.89138,
+      "lng": -55.22901
+    },
+    {
+      "n": "La Barra",
+      "lat": -34.90895,
+      "lng": -54.8515
+    },
+    {
+      "n": "José Ignacio",
+      "lat": -34.68799,
+      "lng": -54.71453
+    },
+    {
+      "n": "Manantiales",
+      "lat": -34.90285,
+      "lng": -54.82767
+    }
+  ],
+  "montevideo": [
+    {
+      "n": "Montevideo",
+      "lat": -34.81807,
+      "lng": -56.21371
+    },
+    {
+      "n": "Pocitos",
+      "lat": -34.81807,
+      "lng": -56.21371
+    },
+    {
+      "n": "Punta Carretas",
+      "lat": -34.81807,
+      "lng": -56.21371
+    },
+    {
+      "n": "Ciudad Vieja",
+      "lat": -34.81807,
+      "lng": -56.21371
+    },
+    {
+      "n": "Carrasco",
+      "lat": -34.81807,
+      "lng": -56.21371
+    },
+    {
+      "n": "Malvín",
+      "lat": -34.81807,
+      "lng": -56.21371
+    },
+    {
+      "n": "Buceo",
+      "lat": -34.81807,
+      "lng": -56.21371
+    }
+  ],
+  "paysandu": [
+    {
+      "n": "Paysandú",
+      "lat": -32.31642,
+      "lng": -58.07779
+    },
+    {
+      "n": "Guichón",
+      "lat": -32.35836,
+      "lng": -57.19928
+    },
+    {
+      "n": "Quebracho",
+      "lat": -31.93522,
+      "lng": -57.89933
+    }
+  ],
+  "rio-negro": [
+    {
+      "n": "Fray Bentos",
+      "lat": -33.12468,
+      "lng": -58.30147
+    },
+    {
+      "n": "Young",
+      "lat": -32.70417,
+      "lng": -57.63757
+    },
+    {
+      "n": "Nuevo Berlín",
+      "lat": -32.98027,
+      "lng": -58.04658
+    },
+    {
+      "n": "San Javier",
+      "lat": -32.66498,
+      "lng": -58.13244
+    }
+  ],
+  "rivera": [
+    {
+      "n": "Rivera",
+      "lat": -30.90337,
+      "lng": -55.56756
+    },
+    {
+      "n": "Tranqueras",
+      "lat": -31.19416,
+      "lng": -55.7641
+    },
+    {
+      "n": "Vichadero",
+      "lat": -31.77773,
+      "lng": -54.69186
+    }
+  ],
+  "rocha": [
+    {
+      "n": "Rocha",
+      "lat": -34.47881,
+      "lng": -54.32734
+    },
+    {
+      "n": "Chuy",
+      "lat": -33.69766,
+      "lng": -53.45856
+    },
+    {
+      "n": "Castillos",
+      "lat": -34.19773,
+      "lng": -53.85785
+    },
+    {
+      "n": "Lascano",
+      "lat": -33.67306,
+      "lng": -54.20776
+    },
+    {
+      "n": "La Paloma",
+      "lat": -34.65212,
+      "lng": -54.18218
+    },
+    {
+      "n": "Punta del Diablo",
+      "lat": -34.04041,
+      "lng": -53.55248
+    },
+    {
+      "n": "Velázquez",
+      "lat": -34.03435,
+      "lng": -54.2798
+    }
+  ],
+  "salto": [
+    {
+      "n": "Salto",
+      "lat": -31.38817,
+      "lng": -57.94599
+    },
+    {
+      "n": "Constitución",
+      "lat": -31.07712,
+      "lng": -57.82143
+    },
+    {
+      "n": "Belén",
+      "lat": -30.79622,
+      "lng": -57.76484
+    }
+  ],
+  "san-jose": [
+    {
+      "n": "San José de Mayo",
+      "lat": -34.33836,
+      "lng": -56.71105
+    },
+    {
+      "n": "Libertad",
+      "lat": -34.63354,
+      "lng": -56.62099
+    },
+    {
+      "n": "Ecilda Paullier",
+      "lat": -34.35814,
+      "lng": -57.05168
+    },
+    {
+      "n": "Rodríguez",
+      "lat": -34.38111,
+      "lng": -56.53788
+    }
+  ],
+  "soriano": [
+    {
+      "n": "Mercedes",
+      "lat": -33.25484,
+      "lng": -58.03013
+    },
+    {
+      "n": "Dolores",
+      "lat": -33.52977,
+      "lng": -58.21857
+    },
+    {
+      "n": "Cardona",
+      "lat": -33.86956,
+      "lng": -57.36773
+    },
+    {
+      "n": "Palmitas",
+      "lat": -33.50694,
+      "lng": -57.80101
+    }
+  ],
+  "tacuarembo": [
+    {
+      "n": "Tacuarembó",
+      "lat": -31.72018,
+      "lng": -55.9905
+    },
+    {
+      "n": "Paso de los Toros",
+      "lat": -32.8075,
+      "lng": -56.51744
+    },
+    {
+      "n": "Pueblo del Barro",
+      "lat": -31.96678,
+      "lng": -55.40731
+    }
+  ],
+  "treinta-y-tres": [
+    {
+      "n": "Treinta y Tres",
+      "lat": -33.2384,
+      "lng": -54.37038
+    },
+    {
+      "n": "Vergara",
+      "lat": -32.94314,
+      "lng": -53.9388
+    },
+    {
+      "n": "Santa Clara de Olimar",
+      "lat": -32.92117,
+      "lng": -54.94372
+    }
+  ]
+};
